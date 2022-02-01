e2996088cffa5c0ad9b256b66b775659702dbdf7
poniitty <pekka.niittynen@helsinki.fi>
2022-02-01 16:59:14 +0100

    Clean version

--- scripts/summary_figures.R
+++ scripts/summary_figures.R
@@ -1,7 +1,5 @@
 ###############################################################################
-# This code plots summary figures
-#
-#
+# This code plots some summary figures
 #
 
 library(tidyverse)
@@ -9,6 +7,7 @@ library(lubridate)
 library(cowplot)
 library(viridis)
 library(zoo)
+library(data.table)
 
 # This is needed to plot the dates in English if your system language is something else
 Sys.setlocale("LC_TIME", "english")
@@ -19,6 +18,9 @@ plotting_order <- c("MAL","AIL","VAR","TII","PIS","HYY","KAR")
 
 # check if study areas are as planned
 unique(d$area)
+
+# How many different sites
+length(unique(d$id_code))
 
 
 #######################################################################
@@ -63,74 +65,109 @@ ggsave("visuals/logger_numbers_SUPPL.pdf",
        width = 20, height = 16, units = "cm")
 
 
-##############################################################################
-# Stacked SD barplots
+########################################################################
+# Environmental gradients
 
-d <- read_csv("data/all_data_monthly.csv")
+d <- read_csv("output/model_outputs/model_input_data.csv")
+plotting_order <- c("MAL","AIL","VAR","TII","PIS","HYY","KAR")
+
+your_palette <- colorRampPalette(c("#000099", "#0000FF", "#5000FF", "#9F0FF0", "#EF42BD", "#FF758A", "#FFA857", "#FFDB24"))
+
+
+# kj to kwh
+d %>% mutate(pisr = pisr/3600) %>% 
+  filter(!is.na(area)) -> d
+
+d %>% select(-(T1_mean:T4_meanmin),-hydro_year, -year) %>% 
+  filter(month %in% c(1,7)) %>% 
+  mutate(month = recode_factor(month, 
+                               "1" = "Jan",
+                               "7" = "Jul")) %>% 
+  relocate(pisr, .after = water_prop_500m) %>% 
+  pivot_wider(id_code:wetland_prop_100m, names_from = month, values_from = pisr, names_prefix = "pisr_") %>% 
+  group_by(id_code, area) %>% 
+  summarise(across(where(is.double), ~mean(.x, na.rm = T))) %>% 
+  pivot_longer(cols = moist_med:pisr_Jul) %>% 
+  mutate(area = factor(area, levels = plotting_order)) %>% 
+  mutate(name = recode_factor(name, altitude = "Elevation (m)",
+                              pisr_Jan = "Solar radiation, January (kWh / m²)",
+                              pisr_Jul = "Solar radiation, July (kWh / m²)",
+                              canopy_cover2m_10m = "Canopy cover (%)",
+                              snow_days = "Snow cover duration (days)",
+                              tpi100 = "Topographic position index",
+                              moist_med = "Soil moisture (VWC%)",
+                              wetland_prop_100m = "Wetland cover (%)",
+                              water_prop_500m = "Water cover (%)")) -> d
+
+d %>% 
+  ggplot(aes(x = area, y = value, fill = area, color = area)) +
+  geom_violin(scale = "width")+
+  stat_summary(fun=median, geom='point', color = "gray80", size = 1.5, show.legend = F) +
+  facet_wrap(vars(name), scales = "free")+
+  scale_fill_manual(values = your_palette(7)) +
+  scale_color_manual(values = your_palette(7)) +
+  theme_bw()+
+  xlab(NULL)+ylab(NULL) -> gg1
+
+ggsave("visuals/Env_gradients_violin.pdf",
+       gg1,
+       width = 25, height = 18, units = "cm")
+
+##########################################################################
+# THE REST ARE ONLY PRELIMINARY PLOTTINGS
+
+##############################################################################
+# SD time series
+# This is needed to plot the dates in English if your system language is something else
+Sys.setlocale("LC_TIME", "english")
 
 plotting_order <- c("MAL","AIL","VAR","TII","PIS","HYY","KAR")
-month_order <- as.character(as.yearmon(seq(as_date("2019-11-01"), as_date("2020-10-31"), by = "month"), format = "%b/%Y"))
 
-str_var <- function(x,no){ unlist(lapply(x, function(x){ strsplit(x,"_")[[1]][no]}))}
+d <- fread("C:/datacloud/biogeoclimate/microclimate/data/logger/data_article/all_data.csv")
 
-d %>% select(-starts_with("moist"),-ends_with("_meanmax"),
-             -ends_with("_meanmin"),-ends_with("_prop")) %>% 
-  pivot_longer(cols = T1_mean:T4_absmin, names_to = "var") %>% 
-  mutate(var2 = str_var(var, 2),
-         var = str_var(var, 1)) %>%
-  filter(is.finite(value)) -> d
+d %>% mutate(datetime = with_tz(datetime, tzone = "Etc/GMT-2")) -> d
 
-d %>% group_by(area, year, month, var, var2) %>% 
-  summarise(tsd = sd(value)) -> dsd
+d %>% group_by(area, datetime) %>% 
+  summarise(T1 = sd(T1, na.rm = T),
+            T2 = sd(T2, na.rm = T),
+            T3 = sd(T3, na.rm = T),
+            T4 = sd(T4, na.rm = T)) %>% 
+  filter(!is.na(T4)) -> d
 
-# Within-area SD stacked
-dsd %>% mutate(area = factor(area, levels = plotting_order)) %>% 
-  ggplot(aes(fill=area, y=tsd, x=var)) + 
-  geom_bar(position="stack", stat="identity")+
-  scale_fill_manual(values = viridis(7))+
-  facet_grid(cols = vars(month), rows = vars(var2)) + 
-  theme_minimal()+
-  theme(panel.spacing = unit(0.1, "lines")) +
-  ylab("Stacked monthly within-area standard deviation") +
-  xlab("Temperature variable")
+d %>% 
+  mutate(area = factor(area, levels = plotting_order)) %>% 
+  pivot_longer(cols = T1:T4, names_to = "tsen", values_to = "Temperature") %>% 
+  ggplot(aes(x = datetime))+
+  geom_line(aes(y = Temperature), color = "orange")+
+  geom_smooth(aes(y = Temperature), span = 0.3, method = "loess", se = T, color = "black")+
+  facet_grid(rows = vars(area), cols = vars(tsen)) +
+  scale_x_datetime(breaks = as_datetime(c("2019-11-01 01:00:00","2020-02-01 01:00:00",
+                                      "2020-05-01 01:00:00","2020-08-01 01:00:00"), tz = "Etc/GMT-2"),
+               date_labels =  "%b") +
+  ylab("Standard deviation (Temperature)") + xlab(NULL)+
+  theme_bw() -> gg1
 
+d %>% 
+  filter(month(datetime) == 7) %>% 
+  mutate(area = factor(area, levels = plotting_order)) %>% 
+  pivot_longer(cols = T1:T4, names_to = "tsen", values_to = "Temperature") %>% 
+  ggplot(aes(x = datetime))+
+  geom_line(aes(y = Temperature), color = "orange")+
+  geom_smooth(aes(y = Temperature), span = 0.3, method = "loess", se = T, color = "black")+
+  facet_grid(rows = vars(area), cols = vars(tsen)) +
+  scale_x_datetime(breaks = as_datetime(c("2020-07-01 01:00:00","2020-07-11 01:00:00",
+                                          "2020-07-21 01:00:00"), tz = "Etc/GMT-2"),
+                   date_labels =  "%b %d") +
+  ylab("Standard deviation (Temperature)") + xlab(NULL)+
+  theme_bw() -> gg2
 
-# How the within-area  variability compares between areas
-dsd %>% mutate(area = factor(area, levels = plotting_order)) %>% 
-  mutate(ym = ym(paste0(year, "-", month))) %>% 
-  mutate(ym = factor(as.yearmon(ym, format = "%b/%Y"), levels = month_order)) %>% 
-  ggplot(aes(fill=area, y=tsd, x=var)) + 
-  geom_bar(position="fill", stat="identity")+
-  scale_fill_manual(values = viridis(7))+
-  facet_grid(cols = vars(ym), rows = vars(var2)) + 
-  theme_minimal()+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5, size = 9))+
-  theme(panel.spacing = unit(0.1, "lines")) +
-  ylab(NULL) + xlab("Temperature sensor")
+ggsave("visuals/sd_across_areas_year_SUPPL.pdf",
+       gg1,
+       width = 20, height = 25, units = "cm")
 
-
-dsd %>% mutate(area = factor(area, levels = plotting_order)) %>% 
-  mutate(ym = ym(paste0(year, "-", month))) %>% 
-  ggplot(aes(fill=area, y=tsd, x=ym)) + 
-  geom_bar(position="stack", stat="identity")+
-  scale_fill_manual(values = viridis(7))+
-  facet_grid(cols = vars(var), rows = vars(var2)) + 
-  theme_cowplot()+
-  scale_x_date(date_breaks = "3 month", date_labels =  "%b %Y") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5, size = 10))+
-  ylab("Stacked monthly within-area standard deviation") +
-  xlab(NULL)
-
-dsd %>% mutate(area = factor(area, levels = plotting_order)) %>% 
-  mutate(ym = ym(paste0(year, "-", month))) %>% 
-  ggplot(aes(fill=area, y=tsd, x=ym)) + 
-  geom_bar(position="fill", stat="identity")+
-  scale_fill_manual(values = viridis(7))+
-  facet_grid(cols = vars(var), rows = vars(var2)) + 
-  theme_cowplot()+
-  scale_x_date(date_breaks = "3 month", date_labels =  "%b %Y") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5, size = 10))+
-  xlab(NULL) + ylab(NULL)
+ggsave("visuals/sd_across_areas_July_SUPPL.pdf",
+       gg2,
+       width = 20, height = 25, units = "cm")
 
 
 #####################################################################
@@ -144,23 +181,59 @@ plotting_order <- c("MAL","AIL","VAR","TII","PIS","HYY","KAR")
 month_order <- as.character(as.yearmon(seq(as_date("2019-11-01"), as_date("2020-10-31"), by = "month"), format = "%b/%Y"))
 
 d %>% 
-  ggplot(aes(y = T1_absmin, x = T1_absmax, color = month)) +
+  ggplot(aes(y = T1_absmin, x = T1_absmax, color = month, group = month)) +
   geom_point() +
+  # geom_smooth(method = "lm")+
   scale_color_gradientn(colours = kovesi.cyclic_mrybm_35_75_c68_s25(12))
 
 d %>% 
-  ggplot(aes(y = T2_absmin, x = T2_absmax, color = month)) +
+  ggplot(aes(y = T2_absmin, x = T2_absmax, color = month, group = month)) +
   geom_point() +
+  geom_smooth(method = "lm")+
   scale_color_gradientn(colours = kovesi.cyclic_mrybm_35_75_c68_s25(12))
 
 d %>% 
-  ggplot(aes(y = T3_absmin, x = T3_absmax, color = month)) +
+  ggplot(aes(y = T3_absmin, x = T3_absmax, color = month, group = month)) +
   geom_point() +
+  geom_smooth(method = "lm") +
   scale_color_gradientn(colours = kovesi.cyclic_mrybm_35_75_c68_s25(12))
 
 d %>% 
-  ggplot(aes(y = T4_absmin, x = T4_absmax, color = month)) +
+  ggplot(aes(y = T4_absmin, x = T4_absmax, color = month, group = month)) +
   geom_point() +
+  geom_smooth(method = "lm") +
   scale_color_gradientn(colours = kovesi.cyclic_mrybm_35_75_c68_s25(12))
 
-d %>% filter(T4_absmin < -30) %>% data.table::as.data.table()
+d %>% 
+  ggplot(aes(y = T1_mean, x = T3_mean, color = month, group = month)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  scale_color_gradientn(colours = kovesi.cyclic_mrybm_35_75_c68_s25(12))
+
+d %>% 
+  ggplot(aes(y = T1_absmin, x = T4_absmin, color = area, group = area)) +
+  geom_point(size = 0.7) +
+  geom_smooth(method = "lm") +
+  scale_color_manual(values = kovesi.cyclic_mrybm_35_75_c68_s25(7))
+
+d %>% 
+  ggplot(aes(y = T1_absmax, x = T4_absmax, color = area, group = area)) +
+  geom_point(size = 0.7) +
+  geom_smooth(method = "lm") +
+  scale_color_manual(values = kovesi.cyclic_mrybm_35_75_c68_s25(7))
+
+d %>% mutate(area = factor(area, levels = plotting_order)) %>% 
+  ggplot(aes(y = T1_mean, x = T4_mean)) +
+  geom_point(aes(color = area),size = 0.7) +
+  geom_smooth(method = "lm", color = "black") +
+  geom_smooth(aes(color = area, group = area), method = "lm") +
+  facet_wrap(vars(month), scales = "free")+
+  scale_color_manual(values = kovesi.rainbow_bgyr_35_85_c72(7))
+
+d %>% mutate(area = factor(area, levels = plotting_order)) %>% 
+  ggplot(aes(y = T1_absmin, x = T1_absmax)) +
+  geom_point(aes(color = area),size = 0.7) +
+  geom_smooth(method = "lm", color = "black") +
+  geom_smooth(aes(color = area, group = area), method = "lm") +
+  facet_wrap(vars(month), scales = "free")+
+  scale_color_manual(values = kovesi.rainbow_bgyr_35_85_c72(7))
